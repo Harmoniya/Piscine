b21bc79232f6eaa7a4aabc80b0697d7b9e5d3288
Harmoniya <rcausse54@gmail.com>
2022-08-24 10:34:55 +0200

    first commi

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
+void	ft_putchar(char c);
 
-void ft_putchar(char c);
 {
 	write(1, &c, 1);
 }
 
-int main()
+int	main(void)
 {
-	ft_putchar(c);
+	ft_putchar('c');
 	return(0);
 }
